05929ff067da7b5b22b5d6756bbaccf9cf518452
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-06 19:57:20 -0800

    Add methods to session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,24 @@
 class Api::SessionsController < ApplicationController
+
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+        if @user
+            login(@user)
+
+            render json: 'logged in', status: 200
+        else
+            render json: ['Invalid credentials'], status: 401
+        end
+    end
+
+    def destroy
+        if logged_in?
+            logout
+            render json: 'logged out', status: 200
+        else
+            render json: 'No one is logged in', status: 404
+        end
+    end
+
 end
