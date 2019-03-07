08f2ba813a02ed6a157583406ef7dc935a34119c
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-07 14:55:03 -0800

    Add root component

---
+++ frontend/components/root.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter } from 'react-router-dom';
+
+const Root = (props) => {
+
+    return (
+        <Provider>
+            <HashRouter>
+                
+            </HashRouter>
+        </Provider>
+    )
+};
+
+export default Root;
