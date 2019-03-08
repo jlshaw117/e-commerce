b55b32577f1c106d38cad31144859bf0e6dc34ad
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-08 14:09:15 -0800

    Set a default state in session errors reducer

--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,6 +1,6 @@
 import { RECIEVE_SESSION_ERRORS, RECIEVE_USER} from '../actions/session_actions';
 
-export default (state, action) => {
+export default (state = [], action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECIEVE_SESSION_ERRORS:
