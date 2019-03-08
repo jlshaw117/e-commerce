7aa88187f4477085278317e40e465ee89084ed0a
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-07 18:13:14 -0800

    Add utils to window for testing

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -2,21 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
+import {login, signup, logout} from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-
-    const root = getElementByID('root');
+    
+    const root = document.getElementById('root');
     let store;
-    if (window.currentUser) {
-        let preLoadedState = {
-            session: {currentUser: true}
-        };
+    // if (window.currentUser) {
+    //     let preLoadedState = {
+    //         session: {currentUser: true}
+    //     };
 
-        store = configureStore(preLoadedState);
-        delete window.currentUser;
-    } else {
-        store = configureStore();
-    }
+    //     store = configureStore(preLoadedState);
+    //     delete window.currentUser;
+    // } else {
+    //     store = configureStore();
+    // }
 
-    ReactDOM.render(<Root store={store}/>, root);
+    // ReactDOM.render(<Root store={store}/>, root);
+    ReactDOM.render(<h1>Testing</h1>, root);
+    // Testing Begin
+        window.login = login;
+        window.logout = logout;
+        window.signup = signup;
+    //Testing End
 });
