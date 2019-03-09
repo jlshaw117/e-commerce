eb9918f4db33fa6255c58a4e5487db2d8cde6cec
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-08 22:55:40 -0800

    Fix bug with json response

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -15,9 +15,9 @@ class Api::SessionsController < ApplicationController
     def destroy
         if logged_in?
             logout
-            render json: 'logged out', status: 200
+            render json: {}, status: 200
         else
-            render json: 'No one is logged in', status: 404
+            render json: ['No one is logged in'], status: 404
         end
     end
 
