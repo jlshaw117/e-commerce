86a68960a503d834f1f09fb1725ed22693456cda
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-08 23:03:07 -0800

    Import header component to app

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import HeaderContainer from './header/header_container';
 
 export default () => {
     return (
         <div>
+            <HeaderContainer/>
             <h1>React Is Working!!!</h1>
         </div>
     )
