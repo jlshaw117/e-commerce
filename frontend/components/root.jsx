740947f151a3b87726ebcdff55c493fa33b53e05
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-08 20:04:28 -0800

    Import app into root component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import App from './app';
 
 const Root = (props) => {
 
     return (
         <Provider>
             <HashRouter>
-                
+                <App/>
             </HashRouter>
         </Provider>
     )
