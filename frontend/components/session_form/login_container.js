c231a283f28a13de495b5914456ca502ae3f4c94
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-09 16:10:12 -0800

    Add login container

---
+++ frontend/components/session_form/login_container.js
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import SessionForm from './session_form';
+import { login } from '../../actions/session_actions';
+
+const mapStateToProps = (state) => {
+
+    return ({
+        user: {
+            email: '',
+            password: ''
+        },
+        formType: 'Login'
+    });
+};
+
+const mapDispatchToProps = (dispatch) => {
+
+    return ({
+        action: (user) => dispatch(login(user))
+    });
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(SessionForm);
