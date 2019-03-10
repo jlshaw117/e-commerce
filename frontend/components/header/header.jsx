3a4e1cf21b481568c1b42776c9d6cef712eccea2
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-09 16:57:32 -0800

    Add first name to header

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -5,6 +5,7 @@ export default ({currentUser, logout}) => {
     if (currentUser) {
         return (
             <div>
+                <h1>Hello {currentUser.f_name}</h1>
                 <button className='logout-button' onClick={logout}>Logout</button>
             </div>
         )
