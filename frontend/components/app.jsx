4bacb331ac9f11dd4f0b9e613e8db793f88ed877
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-09 17:06:23 -0800

    Add routes to login and signup

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import HeaderContainer from './header/header_container';
+import { Route } from 'react-router-dom';
+import LoginFormContainer from './session_form/login_container';
+import SignUpFormContainer from './session_form/signup_container';
 
 export default () => {
     return (
         <div>
             <HeaderContainer/>
             <h1>React Is Working!!!</h1>
+
+            <Route exact path='/login' component={LoginFormContainer}/>
+            <Route exact path='/signup' component={SignUpFormContainer}/>
         </div>
     )
 };
