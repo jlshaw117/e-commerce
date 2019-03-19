29b23c63502c4f8ec762f644b81383904fe9c95b
Jerrik Shaw <jlshaw117@gmail.com>
2019-03-19 16:56:38 -0700

    Add preloaded state for current user

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -2,28 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import {login, signup, logout} from './util/session_api_util';
+// import {login, signup, logout} from './util/session_api_util';
+import {login, signup, logout} from './actions/session_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
     
     const root = document.getElementById('root');
     let store;
-    // if (window.currentUser) {
-    //     let preLoadedState = {
-    //         session: {currentUser: true}
-    //     };
+    if (window.currentUser) {
+        let preLoadedState = {
+            session: {currentUser: window.currentUser}
+        };
 
-    //     store = configureStore(preLoadedState);
-    //     delete window.currentUser;
-    // } else {
-    //     store = configureStore();
-    // }
+        store = configureStore(preLoadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
 
-    // ReactDOM.render(<Root store={store}/>, root);
-    ReactDOM.render(<h1>Testing</h1>, root);
+    ReactDOM.render(<Root store={ store }/>, root);
     // Testing Begin
         window.login = login;
         window.logout = logout;
         window.signup = signup;
+        window.getState = store.getState;
+        window.dispatch = store.dispatch;
     //Testing End
 });
